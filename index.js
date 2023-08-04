b300121a1185973372e7c84221decaf38cfdb5d0
Gabrielaounao <133013470+Gabrielaounao@users.noreply.github.com>
2023-08-04 17:01:29 +0000

    add

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+//index.js
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
+import { selectUsuarios } from "./bd.js";
+
+dotenv.config();
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -7,6 +13,16 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
     nome: "Gabriela de Souza Reis",      // Substitua pelo seu nome
   });
   console.log("Rota / solicitada");
+});
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
