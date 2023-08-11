0fbbf622e9786cdd750526e8554a480e33d82dea
Gabrielaounao <133013470+Gabrielaounao@users.noreply.github.com>
2023-08-11 13:57:09 +0000

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //index.js
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";      // Requisição do pacote do express
 import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";
-dotenv.config();
 
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
